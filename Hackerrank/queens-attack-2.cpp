027284843e0d901f7a89847f10dbe028b90dd3b6
Rona ¯\_(ツ)_/¯ <ronayumik@users.noreply.github.com>
2017-08-11 15:19:07 +0700

    queens-attack-2, still not complete
    30% testcases passed

---
+++ Hackerrank/queens-attack-2.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define ll long
+
+//hackerrank queens-attack-2
+using namespace std;
+ll temp1, temp2, temp3, temp4, temp5, size, obstacles, qx, qy;
+ll limit [8][2];
+
+void pushBack()
+{
+	
+}
+
+void construct()
+{
+		
+}
+
+ll count()
+{
+	int temp;
+}
+int main(){
+	cin >> size >> obstacles;
+	cin >> qx >> qy;
+	for(ll x=0;x< obstacles;x++) pushBack();
+	construct();
+	
+	cout << count() << endl;	
+	
+}
