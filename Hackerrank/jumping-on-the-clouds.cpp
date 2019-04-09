9dfb6cd8786188d32d460de6250e0a3a347a8ba5
Rona <deasnataly@gmail.com>
2019-04-09 11:59:37 +0700

    codejam(foregone): add solution

---
+++ Hackerrank/jumping-on-the-clouds.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int t, N;
+vector<int> p;
+
+int min(int a, int b) {
+    return a < b ? a : b;
+}
+
+int dp(int idx) {
+
+} 
+
+int main() {
+    cin >> N;
+    while(N--) {
+        cin >> t;
+        p.push_back(t);
+    }
+    cout << dp() << endl;
+
+
+    return 0;
+}
