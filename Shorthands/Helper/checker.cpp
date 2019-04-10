92bc660982a05674651dc9b546212902c9e786f5
Rona <deasnataly@gmail.com>
2019-04-10 10:42:19 +0700

    chore(tidying): tidy up project

---
+++ Shorthands/Helper/checker.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	char a;
+	scanf("%d", &a);
+	while(  a!='\n' ){
+		cout << "mantap";
+		scanf("%c", &a);
+	}
+	return 0;
+}
+
+
+
---
+++ Shorthands/no-bits-header.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <cstring>
+#include <queue>
+#include <map>
+#include <cassert>
+#include <sstream>
+#include <set>
+#include <list>
+#include <algorithm>
+#include <math.h>
+#include <cstdlib>
+
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define REPV(i,ar) for(int i=0;i<ar.size();i++)
+#define EACH(it,mp) for(__typeof(mp.begin()) it = mp.begin();it!=mp.end();it++)
+#define sz size()
+#define pb push_back
+#define sor(ar) sort(ar.begin(),ar.end());
+#define INF (1<<30)
+#define LINF ((LL) 1e18)
+#define rev(ar) reverse(ar.begin(),ar.end());
+#define GI ({int t;scanf("%d",&t);t;})
+#define GETN(V) for(int i=GI;i>=0;i--) V.pb( GI )
+using namespace std;
+typedef long long int LL;
+typedef pair<int,int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+
+int main() {
+  cout << "Hello world!" << endl;
+  return 0;
+}
