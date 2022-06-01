5bcd7d6864ed4eedd1633b00a403021520fa7f3b
rona <ronauli@xendit.co>
2022-06-01 15:12:23 +0700

    learn(java): add fizzbuzz

--- my-app/src/main/java/com/mycompany/domain/FizzBuzz.java
+++ my-app/src/main/java/com/mycompany/domain/FizzBuzz.java
@@ -1,9 +1,18 @@
 package com.mycompany.domain;
+import java.util.*;
 
 public class FizzBuzz {
     public static String check(int number) {
+        StringBuilder result = new StringBuilder();
         if(number % 3 == 0) {
-            return "fizz";
+            result.append("fizz");
+        }
+        if(number % 5 == 0) {
+            result.append("buzz");
+        }
+
+        if(result.length() > 0) {
+            return result.toString();
         }
 
         return Integer.toString(number);
--- my-app/src/test/java/com/mycompany/domain/FizzBuzzTest.java
+++ my-app/src/test/java/com/mycompany/domain/FizzBuzzTest.java
@@ -12,11 +12,41 @@ public class FizzBuzzTest
 {
     @Test
     @DisplayName("it should return number when not divisible by 3")
-    void shouldAnswerWithTrue()
+    void shouldReturnStringifiedNumber()
     {
         int input = 4;
         String actual = FizzBuzz.check(input);
         String expected = "4";
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    @DisplayName("it should return fizz when divisible by 3")
+    void shouldReturnFizz()
+    {
+        int input = 3;
+        String actual = FizzBuzz.check(input);
+        String expected = "fizz";
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    @DisplayName("it should return buzz when divisible by 5")
+    void shouldReturnBuzz()
+    {
+        int input = 5;
+        String actual = FizzBuzz.check(input);
+        String expected = "buzz";
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    @DisplayName("it should return fizzbuzz when divisible by 15")
+    void shouldReturnFizzBuzz()
+    {
+        int input = 45;
+        String actual = FizzBuzz.check(input);
+        String expected = "fizzbuzz";
 
         assertEquals(actual, expected);
     }
