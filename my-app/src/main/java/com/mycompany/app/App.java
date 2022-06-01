647045ecb45c90cb616a0a038042dd9af0cd6266
rona <ronauli@xendit.co>
2022-06-01 16:15:59 +0700

    learn(java): final IO

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,12 +1,32 @@
 package com.mycompany.app;
 
-import java.util.Scanner;
+import java.util.*;
+import com.mycompany.domain.IsPalindrome;
 
 public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(App.getGreeting());
+        Scanner scanner = new Scanner(System.in);
+        String result;
+
+        while(true) {
+            System.out.println("Input text to check, EXIT to exit");
+            String input = scanner.nextLine();
+
+            if (input.equals("EXIT")) {
+                break;
+            }
+
+            result = IsPalindrome.check(input)
+                ? "IS PALINDROME"
+                : "NOT PALINDROME";
+
+            System.out.println(result);
+        }
+
+        System.out.println("Exiting ...");
+        scanner.close();
     }
 
     public static String getGreeting() {
