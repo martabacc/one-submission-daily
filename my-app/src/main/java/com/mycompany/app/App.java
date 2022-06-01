17c78ea1823b5c9239de41e46454343f37e93da9
rona <ronauli@xendit.co>
2022-06-01 15:25:47 +0700

    learn(java): finall fizzbuzz

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,15 +1,28 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+import com.mycompany.domain.FizzBuzz;
+
 public class App
 {
     public static void main( String[] args )
     {
-        String greeting = App.getGreeting();
-        System.out.println(greeting);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Til which number you want fizzbuzz to be printed: ");
+        int number = input.nextInt();
+
+        input.close();
+
+        App.printFizzBuzzTilNumber(number);
+    }
+
+    public static void printFizzBuzzTilNumber(int limit) {
+        FizzBuzz logic = new FizzBuzz();
+        for (int i=1;i<=limit;i++) {
+            System.out.print(FizzBuzz.check(i)+ " ");
+        }
+
+        System.out.print("\n");
     }
 
     public static String getGreeting() {
--- my-app/src/main/java/com/mycompany/domain/FizzBuzz.java
+++ my-app/src/main/java/com/mycompany/domain/FizzBuzz.java
@@ -1,4 +1,5 @@
 package com.mycompany.domain;
+
 import java.util.*;
 
 public class FizzBuzz {
